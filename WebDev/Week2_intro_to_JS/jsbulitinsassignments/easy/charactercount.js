746f8987e820356f9957ac2e68654d0991a5d6a7
yashmmith <samindlayashmmithsaii3@gmail.com>
2026-03-02 09:46:03 +0530

    Added assignment.txt

---
+++ WebDev/Week2_intro_to_JS/jsbulitinsassignments/easy/charactercount.js
@@ -0,0 +1,13 @@
+let input= "aabbcc";
+let result={}
+for (let i=0;i<input.length;i++){
+  let char1=input[i];
+  if(result[char1]){
+    result[char1]++;
+  }
+  else{result[char1]=1}
+}
+console.log(result);
+
+:qw
+:wq
